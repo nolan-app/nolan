fa2ebe55eba7b4ad9bdf5e434761288f81256b34
unknown <painn5@yahoo.com>
2019-10-21 04:52:57 -0300

    (style): added styled-components.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import { Container } from '../../styles/Home'
+
 function Home() {
     return (
-        <>
+        <Container>
             <h1>nolan</h1>
             <h4>know when and where</h4>
-        </>
+        </Container>
     );
 }
 
