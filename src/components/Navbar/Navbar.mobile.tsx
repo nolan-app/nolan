c2326e2ec5bfe22e4d0609df9312bdac62c68917
Osvaldo Margato <15236942+margato@users.noreply.github.com>
2019-10-22 21:02:11 -0300

    (refactor): change tab size

--- src/components/Navbar/Navbar.mobile.tsx
+++ src/components/Navbar/Navbar.mobile.tsx
@@ -16,39 +16,32 @@ const NavbarStyle = createGlobalStyle`
 `;
 
 const menu = (
-    <Menu>
-        <Menu.Item key="0">
-            <AccountButtonLinks>Login</AccountButtonLinks>
-        </Menu.Item>
-        <Menu.Item key="1">
-            <AccountButtonLinks>Create New Account</AccountButtonLinks>
-        </Menu.Item>
-    </Menu>
+  <Menu>
+    <Menu.Item key="0">
+      <AccountButtonLinks>Login</AccountButtonLinks>
+    </Menu.Item>
+    <Menu.Item key="1">
+      <AccountButtonLinks>Create New Account</AccountButtonLinks>
+    </Menu.Item>
+  </Menu>
 );
 
 function NavbarMobile() {
-    return (
-        <>
-            <NavbarStyle />
-            <Row className="navbar">
-                <Col xs={20} sm={20} md={20} lg={6} xl={6}>
-                    <Title>Nolan</Title>
-                </Col>
-                <Col
-                    className="right-button"
-                    xs={4}
-                    sm={4}
-                    md={4}
-                    lg={6}
-                    xl={6}
-                >
-                    <Dropdown overlay={menu} trigger={["click"]}>
-                        <AccountButton type="user" />
-                    </Dropdown>
-                </Col>
-            </Row>
-            <SearchBar />
-        </>
-    );
+  return (
+    <>
+      <NavbarStyle />
+      <Row className="navbar">
+        <Col xs={20} sm={20} md={20} lg={6} xl={6}>
+          <Title>Nolan</Title>
+        </Col>
+        <Col className="right-button" xs={4} sm={4} md={4} lg={6} xl={6}>
+          <Dropdown overlay={menu} trigger={["click"]}>
+            <AccountButton type="user" />
+          </Dropdown>
+        </Col>
+      </Row>
+      <SearchBar />
+    </>
+  );
 }
 export { NavbarMobile };
